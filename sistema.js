267881a2d1fe919e9ec8e4f0988ac83180d0a55c
natalia <nataliamikaellen@hotmail.com>
2021-12-10 22:13:13 -0300

    SistemaAluno

---
+++ sistema.js
@@ -0,0 +1,42 @@
+let nomeDeAluno = ["Marcel", "Franklin", "Rita", "Afrânio"]
+
+for (let nome of nomeDeAluno) {
+    console.log(`Esta pessoa se chama ${nome}`)    
+}
+
+
+
+let numeroDeAlunos = 10;
+
+let contador = 0;
+
+while( contador <= numeroDeAlunos) {
+     //console.log(contador)
+
+     if(contador == 0){
+         console.log("O número atual é zero")
+     }else if(contador % 2 != 0){
+         console.log("O número " + contador + " é impar")
+     }else{
+         console.log(`O número ${contador} é par`)
+     }
+     contador++;       
+ }
+
+
+
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     //console.log(contador)
+
+//     if(contador == 0){
+//         console.log("O número atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("O número " + contador + " é par")
+//     }else{
+//         console.log(`O número ${contador} é impar`)
+//     }       
+// }
+
+
+
